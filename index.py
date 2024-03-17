eda6f64efcf35e9e4875fdf67f6b97bd8b9911a8
AlexGrohmann <alex.grohmann@googlemail.com>
2024-03-17 16:54:24 +0100

    change to python project

---
+++ index.py
@@ -0,0 +1,6 @@
+print("Hello I am now in python")
+print("I am a simple script connecting to mongoDB for future ideas")
+print(
+    "TODO: should it calculate Stuff? Umstellen nach einem Parameter wäre lecker schmecker"
+)
+# https://www.w3schools.com/python/python_mongodb_getstarted.asp
